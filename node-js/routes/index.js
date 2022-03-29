0bafcf0e7f7ae81192baa562fefa8ae2c6bd1725
bhrt249 <78458606+bhrt249@users.noreply.github.com>
2022-03-29 18:32:47 +0000

    Update index.js

--- node-js/routes/index.js
+++ node-js/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'new cloud' });
+  res.render('index', { title: 'new cloud ayo' });
 });
 
 module.exports = router;
